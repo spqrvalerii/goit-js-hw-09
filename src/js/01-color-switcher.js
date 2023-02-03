40ca957b7fe580b8f2326d5c71810658b89463dc
spqrvalerii <1982elcondorpasa@gmail.com>
2023-02-03 13:16:52 +0200

    update 01.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+stopBtn.disabled = true;
 
+let timer = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+startBtn.addEventListener('click', () => {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+
+  timer = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timer);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+});
